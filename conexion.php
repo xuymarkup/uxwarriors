6de7fe9fc63438b62c07f9dfffe3ea5e36aeb62d
jose morales <crozzager@yahoo.com.mx>
2023-04-09 21:59:28 -0600

    fix varaible

--- conexion.php
+++ conexion.php
@@ -6,7 +6,7 @@ $contenido = parse_ini_file($archivo, true);
 
 $DB_HOST = $contenido["DB_HOST"];
 
-$servername = DB_HOST;
+$servername = $DB_HOST;
 $username = "tiendac2_test";
 $password = "password_db@";
 $dbname = "tiendac2_test";
