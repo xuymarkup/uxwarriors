374e6b1624333c11050a93369d818c5da08ac203
jose morales <crozzager@yahoo.com.mx>
2023-04-09 20:39:24 -0600

    add: carrito to menu

--- menu.php
+++ menu.php
@@ -3,8 +3,9 @@
         <li><a href="inicio.php">Inicio</a></li>
         <li><a href="registro.php">Registro</a></li>
         <li><a href="servicios.php">Servicios</a></li>
-        <li><a href="perfil.php">Mi perfil</a></li>
         <li><a href="contacto.php">Contacto</a></li>
+        <li style="float:right"><a href="perfil.php">Mi perfil</a></li>
 		<li style="float:right"><a href="cerrar_sesion.php">Cerrar sesión</a></li>
+        <li style="float:right"><a href="cerrar_sesion.php">Cerrar sesión</a></li>
     </ul>
 </nav>
